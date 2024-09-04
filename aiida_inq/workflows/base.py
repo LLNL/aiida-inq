030c60c298d9d3c825391a33792d4ba43cedb2d1
Keilbart <keilbart1@fireman.llnl.gov>
2024-09-04 09:33:44 -0700

    Implementing builder protocol for base workflow.

--- aiida_inq/workflows/base.py
+++ aiida_inq/workflows/base.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 """Base workchain to run an INQ calculation."""
 
+from aiida import orm
 from aiida.common import AttributeDict
 from aiida.engine import BaseRestartWorkChain, while_
 from aiida.plugins import CalculationFactory
 
+from ..protocols.utils import ProtocolMixin # type: ignore
+
 InqCalculation = CalculationFactory('inq.inq')
 
 
-class InqBaseWorkChain(BaseRestartWorkChain):
+class InqBaseWorkChain(ProtocolMixin, BaseRestartWorkChain):
     """
     Workchain to run an Inq calculation with automated error handling 
     and restarts.
@@ -20,7 +23,22 @@ class InqBaseWorkChain(BaseRestartWorkChain):
     def define(cls, spec):
 
         super(InqBaseWorkChain, cls).define(spec)
-        spec.expose_inputs(InqCalculation, namespace='inq')
+        spec.expose_inputs(
+            InqCalculation, 
+            namespace='inq'
+        )
+        spec.input(
+            'kpoints',
+            valid_type = orm.KpointsData,
+            required = False,
+            help = ''
+        )
+        spec.input(
+            'kpoints_spacing',
+            valid_type = orm.Float,
+            required = False,
+            help = ''
+        )
 
         spec.outline(
             cls.setup,
@@ -32,6 +50,13 @@ class InqBaseWorkChain(BaseRestartWorkChain):
         )
 
         spec.expose_outputs(InqCalculation)
+
+    @classmethod
+    def get_protocol_filepath(cls):
+        from importlib_resources import files
+
+        from ..protocols import inq as protocols # type: ignore
+        return files(protocols) / 'base.yaml'
 
     def setup(self):
         """
@@ -45,3 +70,41 @@ class InqBaseWorkChain(BaseRestartWorkChain):
         super(InqBaseWorkChain, self).setup()
         self.ctx.inputs = AttributeDict(
             self.exposed_inputs(InqCalculation, 'inq'))
+        
+    def get_builder_from_protocol(
+        cls,
+        code,
+        structure,
+        protocol = None,
+        overrides = None,
+        options = None,
+        **kwargs
+    ):
+        """
+        Return a builder prepopulated with inputs based on a provided 
+        protocol. If no protocol is given, the default protocol is set 
+        as moderate.
+
+        :param code: 
+            The ``Code`` instance configured for the ``inq.inq`` plugin.
+        :param structure:
+            The ``StructureData`` instance to use.
+        :param protocol:
+            Protocol to use. Options are moderate, precise, or fast.
+        :param overrides:
+            Optional dictionary of inputs that will override the values 
+            provided from the protocol file.
+        :param options:
+            A dictionary of options that will be recursively set for the 
+            ``metadata.options`` input of all the ``CalcJobs`` that are 
+            nested in this work chain.
+
+        :return:
+            A builder instance with all the inputs defined and ready to 
+            launch.
+        """
+
+        inputs = cls.get_protocol_inputs(protocol, overrides)
+        cutoff = cls.suggested_energy_cutoff(protocol, inputs.pseudo_set)
+
+        return
